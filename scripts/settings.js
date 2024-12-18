02a79d94142baef7b10cb378e3cc2c3e221d2d30
BenABaron <benabaron@gmail.com>
2024-12-18 14:02:38 -0600

    set default to small header

--- scripts/settings.js
+++ scripts/settings.js
@@ -36,7 +36,7 @@ export const registerSettings = () => {
     scope: 'client',
     config: true,
     type: String,
-    default: 'CHAR_HEADER',
+    default: 'CHAR_HEADER_SMALL',
     choices: {
       CHAR_HEADER: game.i18n.localize(
         'PF2E_LEVEL_UP_WIZARD.settings.buttonPlacement.options.charHeaderLarge'
