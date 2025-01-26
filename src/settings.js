8fc1b9db8cdc5863d5b9b35fd86698db72482e28
BenABaron <benabaron@gmail.com>
2025-01-26 17:20:49 -0600

    xp enforcement

--- src/settings.js
+++ src/settings.js
@@ -129,6 +129,19 @@ export const registerSettings = () => {
     default: false
   });
 
+  game.settings.register(module_name, 'xp-enforcement', {
+    name: game.i18n.localize(
+      'PF2E_LEVEL_UP_WIZARD.settings.xpEnforcement.name'
+    ),
+    hint: game.i18n.localize(
+      'PF2E_LEVEL_UP_WIZARD.settings.xpEnforcement.hint'
+    ),
+    scope: 'world',
+    config: true,
+    type: Boolean,
+    default: false
+  });
+
   game.settings.register(module_name, 'additional-feat-compendiums', {
     name: game.i18n.localize(
       'PF2E_LEVEL_UP_WIZARD.settings.additionalCompendiums.name'
